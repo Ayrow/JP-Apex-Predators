639a6f19b5847cf641bb5ef6652ce5ec9bd2a44b
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-20 13:59:50 +0100

    added navigation to full size predator image on tap

--- JP Apex Predators/PredatorDetail.swift
+++ JP Apex Predators/PredatorDetail.swift
@@ -11,60 +11,72 @@ struct PredatorDetail: View {
     let predator: ApexPredator
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .trailing) {
-                Image(predator.type)
-                    .resizable()
-                    .scaledToFit()
-                
-                Image(predator.name.lowercased().filter { $0 != " " })
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: UIScreen.main.bounds.width / 1.5, height: UIScreen.main.bounds.height / 4)
-                    .shadow(color: .black, radius: 6, x: 0, y: 0)
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .trailing) {
+                    Image(predator.type)
+                        .resizable()
+                        .scaledToFit()
+                    
+                    NavigationLink(destination: Image(predator.name.lowercased().filter { $0 != " " })
+                        .resizable()
+                        .scaledToFit()
+                        .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0)
+                        )
+                        ,
+                        label: {
+                        Image(predator.name.lowercased().filter { $0 != " " })
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: UIScreen.main.bounds.width / 1.5, height: UIScreen.main.bounds.height / 4)
+                            .shadow(color: .black, radius: 6, x: 0, y: 0)
+                            
+                            .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0)
+                            )
+                    })
                     .offset(y: -210)
-                    .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
-                
-                VStack(alignment: .leading) {
-                    Text(predator.name)
-                        .font(.largeTitle)
-                        .padding(.bottom, 8)
                     
-                    Text("Appears in: ")
-                        .font(.title3)
-                    
-                    ForEach(predator.movies, id: \.self) {
-                        movie in
-                        Text("•" + movie)
-                            .font(.subheadline)
-                    }
-                    
-                    Text("Movie moments")
-                        .font(.title)
-                        .padding(.top, 15)
-                    
-                    ForEach(predator.movieScenes) {
-                        scene in
-                        Text(scene.movie)
-                            .font(.title2)
-                            .padding([.top, .bottom], 1)
+                    VStack(alignment: .leading) {
+                        Text(predator.name)
+                            .font(.largeTitle)
+                            .padding(.bottom, 8)
                         
-                        Text(scene.sceneDescription)
-                            .padding(.bottom, 15)
+                        Text("Appears in: ")
+                            .font(.title3)
+                        
+                        ForEach(predator.movies, id: \.self) {
+                            movie in
+                            Text("•" + movie)
+                                .font(.subheadline)
+                        }
+                        
+                        Text("Movie moments")
+                            .font(.title)
+                            .padding(.top, 15)
+                        
+                        ForEach(predator.movieScenes) {
+                            scene in
+                            Text(scene.movie)
+                                .font(.title2)
+                                .padding([.top, .bottom], 1)
+                            
+                            Text(scene.sceneDescription)
+                                .padding(.bottom, 15)
+                        }
+                        
+                        Text("Read More")
+                            .font(.caption)
+                        
+                        Link(predator.link, destination: URL(string: predator.link)!)
+                            .font(.caption)
+                            .foregroundColor(.blue)
                     }
-                    
-                    Text("Read More")
-                        .font(.caption)
-                    
-                    Link(predator.link, destination: URL(string: predator.link)!)
-                        .font(.caption)
-                        .foregroundColor(.blue)
+                    .padding(.top, -230)
+                    .padding()
                 }
-                .padding(.top, -230)
-                .padding()
             }
+            .edgesIgnoringSafeArea(.top)
         }
-        .edgesIgnoringSafeArea(.top)
     }
 }
 
